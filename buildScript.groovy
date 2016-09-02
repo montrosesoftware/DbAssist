cf4e489c0d133b782dc1d4657fa8581192038957
Miroslaw Syzdek <miroslaw.syzdek@montrosesoftware.com>
2016-09-02 11:43:37 +0200

    Added test framework for HBM.
    Merged JPA testcases and tester.

--- buildScript.groovy
+++ buildScript.groovy
@@ -15,6 +15,9 @@ callCleanInstall("DbAssist-hbm-3.3.2.GA")
 callCleanInstall("DbAssist-hbm-3.6.10")
 callCleanInstall("DbAssist-hbm-4.2.21")
 callCleanInstall("DbAssist-hbm-4.3.11")
+callCleanInstall("DbAssist-hbm-5.0.10")
+callCleanInstall("DbAssist-hbm-5.1.1")
+callCleanInstall("DbAssist-hbm-5.2.2")
 
 //Montrose Software's JPA libs
 callCleanInstall("DbAssist-jpa-4.2.21")
@@ -29,11 +32,10 @@ callCleanInstall("DbAssist-jpa-commons")
 //Montrose Software's testing framework
 callCleanInstall("DbAssist-Commons")
 callCleanInstall("MavenBuildAllPlugin")
-callCleanInstall("DbAssist-hbm-tester")
-callCleanInstall("DbAssist-jpa-testcases")
 callCleanInstall("DbAssist-jpa-commons")
 
-callMaven("DbAssist-jpa-tester", "MavenBuildAllPlugin:buildAll")
+callMaven("DbAssist-jpa-testcases", "MavenBuildAllPlugin:buildAll")
+callMaven("DbAssist-hbm-tester", "MavenBuildAllPlugin:buildAll")
 
 
 
