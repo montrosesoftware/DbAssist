8f2b37938be747f990fa7fa459cc41d2d8e2441b
Wiktor Muron <wiktor.muron@montrosesoftware.com>
2016-09-07 15:03:20 +0200

    Modified deploy groovy script

--- deployToStagingRepository.groovy
+++ deployToStagingRepository.groovy
@@ -1,4 +1,4 @@
-def callMaven(path, goals){
+def callMaven(path, goals) {
     def proc = "cmd /c mvn -f ${path}/pom.xml ${goals}".execute()
     proc.waitForProcessOutput(System.out, System.err)
     def exitCode = proc.exitValue()
@@ -10,10 +10,12 @@ def callDeployRelease(path){
     callMaven(path, "clean deploy -P release")
 }
 
-//callDeployRelease("DbAssist-hbm-3.3.2")
+callDeployRelease("DbAssist-hbm-3.3.2")
 callDeployRelease("DbAssist-hbm-3.6.10")
-//callDeployRelease("DbAssist-4.2.21")
-//callDeployRelease("DbAssist-4.3.11")
-//callDeployRelease("DbAssist-5.0.10")
-//callDeployRelease("DbAssist-5.1.1")
-//callDeployRelease("DbAssist-5.2.2.")
+callDeployRelease("DbAssist-4.2.21")
+callDeployRelease("DbAssist-4.3.11")
+callDeployRelease("DbAssist-5.0.10")
+callDeployRelease("DbAssist-5.1.1")
+callDeployRelease("DbAssist-5.2.2")
+
+callDeployRelease("DbAssist-jpa-commons")
