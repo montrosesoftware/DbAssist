2d1b5f450d2ac8ffcd21d95982e157dbe8e99d8a
Wiktor Muron <wiktor.muron@montrosesoftware.com>
2016-11-08 11:20:54 +0100

    Updated deploy groovy script

--- deployToStagingRepository.groovy
+++ deployToStagingRepository.groovy
@@ -1,14 +1,18 @@
-new GroovyShell().parse( new File( 'callMaven.groovy' ) ).with {
+new GroovyShell().parse(new File('callMaven.groovy')).with {
 
     def goals = "clean deploy -P release"
 
-//    callMaven("DbAssist-hbm-3.3.2",  goals)
-//    callMaven("DbAssist-hbm-3.6.10",  goals)
-//    callMaven("DbAssist-4.2.21",  goals)
-//    callMaven("DbAssist-4.3.11",  goals)
-//    callMaven("DbAssist-5.0.10",  goals)
-//    callMaven("DbAssist-5.1.1",  goals)
-//    callMaven("DbAssist-5.2.2",  goals)
+    //from the list below, uncomment the artifact you want to release
+    // to the Maven (Nexus) staging repository and run this script
 
-    callMaven("DbAssist-jpa-commons",  goals)
+//    callMaven("DbAssist-hbm-3.3.2", goals)
+//    callMaven("DbAssist-hbm-3.6.10", goals)
+//    callMaven("DbAssist-4.2.21", goals)
+//    callMaven("DbAssist-4.3.11", goals)
+//    callMaven("DbAssist-5.0.10", goals)
+//    callMaven("DbAssist-5.1.1", goals)
+//    callMaven("DbAssist-5.2.2", goals)
+//
+//    callMaven("DbAssist-jpa-commons", goals)
+
 }
